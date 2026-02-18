b760cd6fe573306901f9fa11bbce38b936b81987
STI-TakoYucky <estrada.lnp@gmail.com>
2026-02-18 18:46:20 +0800

    changed placeholder for card inputs for understandability

--- src/components/FlashcardComponents/FlashcardDialog.tsx
+++ src/components/FlashcardComponents/FlashcardDialog.tsx
@@ -83,11 +83,11 @@ export function FlashcardDialog({ children, handleSubmit, editCardData }: AppCre
           <div className="gap-3 flex flex-col">
               <div>
                 <Label>Front</Label>
-                <Textarea ref={termRef} onKeyDown={handleKeyDown} className="mt-2 max-h-[10rem] !text-base bg-white dark:bg-foreground border-[2px] border-dark-200 resize-none overflow-auto" placeholder="Mitochondria" value={term} onChange={(e) => setTerm(e.target.value)}/>
+                <Textarea ref={termRef} onKeyDown={handleKeyDown} className="mt-2 max-h-[10rem] !text-base bg-white dark:bg-foreground border-[2px] border-dark-200 resize-none overflow-auto" placeholder="What is the powerhouse of the cell?" value={term} onChange={(e) => setTerm(e.target.value)}/>
               </div>
               <div>
                 <Label>Back</Label>
-                <Textarea onKeyDown={handleKeyDown} className="mt-2 max-h-[10rem] resize-none overflow-auto !text-base bg-white dark:bg-foreground border-[2px] border-dark-200" placeholder="The powerhouse of the cell." value={definition} onChange={(e) => setDefinition(e.target.value)}/>
+                <Textarea onKeyDown={handleKeyDown} className="mt-2 max-h-[10rem] resize-none overflow-auto !text-base bg-white dark:bg-foreground border-[2px] border-dark-200" placeholder="Mitochondria" value={definition} onChange={(e) => setDefinition(e.target.value)}/>
               </div>
 
               <div className="mt-4 gap-2 flex flex-col">
